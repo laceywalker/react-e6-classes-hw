8d4b4a4928aa047aa9a841896d70c22a590523fe
lacey <hiu@example.com>
2019-03-02 12:39:22 +0000

    finish MVP

--- client/src/models/instrument_families.js
+++ client/src/models/instrument_families.js
@@ -1,4 +1,4 @@
-const PubSub = require('../helpers/pub_sub.js');
+import PubSub from '../helpers/pub_sub.js';
 
 class InstrumentFamilies {
   constructor(data){
@@ -22,4 +22,5 @@ publishFamilyDetail(selectedIndex) {
 
 
 };
-module.exports = InstrumentFamilies;
+
+export default InstrumentFamilies;
--- client/src/views/instrument_family_view.js
+++ client/src/views/instrument_family_view.js
@@ -1,39 +1,44 @@
-const PubSub = require('../helpers/pub_sub.js');
+import PubSub from '../helpers/pub_sub.js';
 
-const InstrumentFamilyView = function (container) {
-  this.container = container;
-};
+class InstrumentFamilyView {
+  constructor(container){
+    this.container = container;
+  }
 
-InstrumentFamilyView.prototype.bindEvents = function () {
+
+bindEvents() {
   PubSub.subscribe('InstrumentFamilies:selected-family-ready', (evt) => {
     const instrumentFamily = evt.detail;
     this.render(instrumentFamily);
   });
 };
 
-InstrumentFamilyView.prototype.render = function (family) {
+
+render(family) {
   this.container.innerHTML = '';
 
-  const familyName = this.createElement('h2', family.name);
+  const {name, description, instruments} = family;
+
+  const familyName = this.createElement('h2', name);
   this.container.appendChild(familyName);
 
-  const familyDescription = this.createElement('p', family.description);
+  const familyDescription = this.createElement('p', description);
   this.container.appendChild(familyDescription);
 
   const instrumentListTitle = this.createElement('h3', 'Instruments include:');
   this.container.appendChild(instrumentListTitle);
 
-  const instrumentList = this.createInstrumentList(family.instruments);
+  const instrumentList = this.createInstrumentList(instruments);
   this.container.appendChild(instrumentList);
 };
 
-InstrumentFamilyView.prototype.createElement = function (elementType, text) {
+createElement(elementType, text) {
   const element = document.createElement(elementType);
   element.textContent = text;
   return element;
 };
 
-InstrumentFamilyView.prototype.createInstrumentList = function (instruments) {
+createInstrumentList(instruments) {
   const list = document.createElement('ul');
 
   instruments.forEach((instrument) => {
@@ -45,4 +50,6 @@ InstrumentFamilyView.prototype.createInstrumentList = function (instruments) {
   return list;
 };
 
-module.exports = InstrumentFamilyView;
+};
+
+export default InstrumentFamilyView;
